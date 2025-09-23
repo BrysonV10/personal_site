b6df43b7886faa8987b24c39d70ee2739e63d92e
BrysonV10 <brysonv10@github.com>
2025-09-22 20:22:17 -0400

    Updating home page with some more information Removing blog link from navbar for now - add back later when ironed out

--- scripts/home_scripts.js
+++ scripts/home_scripts.js
@@ -19,20 +19,3 @@ document.getElementById("rowpic").addEventListener("mouseleave", ()=> {
 });
 
 
-document.getElementById("runpic").addEventListener("mouseenter", ()=> {
-    console.log("hover")
-    document.getElementById("retro_run_pic").animate([
-        {opacity:"1"},
-        {opacity:"0"}
-    ], {duration: 500});
-    document.getElementById("retro_run_pic").style.opacity=0;
-});
-
-document.getElementById("runpic").addEventListener("mouseleave", ()=> {
-    console.log("hover")
-    document.getElementById("retro_run_pic").animate([
-        {opacity:"0"},
-        {opacity:"1"}
-    ], {duration: 500});
-    document.getElementById("retro_run_pic").style.opacity=1;
-});
