c73e73803ce304dec2dfe1eedf5868c566afbc2d
BrysonV10 <brysonv10@github.com>
2025-09-23 09:01:30 -0400

    Added error handling for analytics charts not loading

--- scripts/stalk-scripts.js
+++ scripts/stalk-scripts.js
@@ -1,1 +1,7 @@
 
+function onChartLoad(ok){
+    if(!ok){
+        const chartContainer = document.getElementById("chart");
+        chartContainer.innerHTML = "<p style='color: red;'>Error loading chart. Please try again later.</p>";
+    }
+}
